c0230f993f7f712f367d9f308d7e5e5ab7ff9340
kitsorfan <kitsorfan@protonmail.com>
2021-03-10 18:53:03 +0200

    add first JWT function
    Copied from online tutorial:
    https://realpython.com/token-based-authentication-with-flask/

--- back-end/DB_queries.py
+++ back-end/DB_queries.py
@@ -6,16 +6,7 @@ import datetime
 from flask_mysqldb import MySQL
 import mysql.connector
 
-#@@@@@@@@@@@@@@@@@@@@@@-- DB conection --@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
-#   we are using the default port 3306
 
-app.config['MYSQL_HOST'] = '127.0.0.1'
-app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = 'sqlpassword'
-app.config['MYSQL_DB'] = 'project_e_lectra'
-app.config['MYSQL_CURSORCLASS'] = 'DictCursor' #optional
-
-mysql = MySQL(app)
 
 def hash_password(password,salt):
     key-hashlib.pbkdf2_hmac('sha256', password.encode('utf-8'),salt, 100000)
--- back-end/api.py
+++ back-end/api.py
@@ -24,6 +24,13 @@
 from flask import Flask, Blueprint, jsonify
 from flask_restful import Api, Resource, reqparse, abort, fields, marshal_with
 import flask_restx
+from flask_mysqldb import MySQL
+import mysql.connector
+
+import os
+import hashlib
+import datetime
+import jwt
 
 
 #@@@@@@@@@@@@@@@@@@@@@@-- Api Creation --@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
@@ -32,6 +39,61 @@ app = Flask(__name__)
 
 if __name__ == "__main__":
 	app.run(debug=True)
+
+
+#@@@@@@@@@@@@@@@@@@@@@@-- DB conection --@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+#   we are using the default port 3306
+
+app.config['MYSQL_HOST'] = '127.0.0.1'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'sqlpassword'
+app.config['MYSQL_DB'] = 'project_e_lectra'
+app.config['MYSQL_CURSORCLASS'] = 'DictCursor' #optional
+
+
+mysql = MySQL(app)
+
+#@@@@@@@@@@@@@@@@@@@@@@-- Password Hashing --@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+
+def hash_password(password,salt):
+    key-hashlib.pbkdf2_hmac('sha256', password.encode('utf-8'),salt, 100000)
+    return key
+
+def create_salt():
+    salt = os.urandom(32)
+    return salt
+
+
+#@@@@@@@@@@@@@@@@@@@@@@-- JWT --#############@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+
+def encode_auth_token(self, user_id):
+    """
+    Generates the Auth Token
+    :return: string
+    """
+    try:
+        payload = {
+            'exp': datetime.datetime.utcnow() + datetime.timedelta(days=0, seconds=5),
+            'iat': datetime.datetime.utcnow(),
+            'sub': user_id
+        }
+        return jwt.encode(
+            payload,
+            app.config.get('SECRET_KEY'),
+            algorithm='HS256'
+        )
+    except Exception as e:
+        return e
+
+
+
+
+#---------------------------------------------------------------------------------------------------------------------
+#---------------------------------------------------------------------------------------------------------------------
+#$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$--- API ENDPOINTS ----$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+#---------------------------------------------------------------------------------------------------------------------
+#---------------------------------------------------------------------------------------------------------------------
+
 
 #@@@@@@@@@@@@@@@@@@@@@@-- DB Healhcheck --@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 #   This is an auxilary endpoint that shows the user that the DB is connected
